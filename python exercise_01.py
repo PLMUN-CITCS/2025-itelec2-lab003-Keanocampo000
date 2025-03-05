64a0b1a9be0f3e9d3cbdd9440f4f2d2d7db4982b
Kean Ken Rick Ocampo <ocampokeankenrick_bsit@plmun.edu.ph>
2025-03-06 00:51:13 +0800

    Submitting Python Week 02 - Session 01 - Exercise 01

---
+++ python exercise_01.py
@@ -0,0 +1,16 @@
+# Ocampo, Kean Ken Rick
+# ITELEC2
+# Laboratory #03 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+print("Integer (count):", 10)
+print("Integer (Count):", 15)
+print("Integer (total_count):", 20)
+print("Decimal:", 3.14)
+print("Text:", "Hello, python!")
+print("Boolean:", True)
+print("None Value:", None)
+
+num1 = 5
+num2 = 3
+print(f"Sum: {num1 + num2:.2f}")
